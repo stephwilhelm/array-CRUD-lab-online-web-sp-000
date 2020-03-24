1ed5623fa792e8eb4ae318108144cc0e08fbf8ac
Steph W <wilhelm.steph@gmail.com>
2020-03-24 22:53:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  favorite_bands = ["Nirvana", "Pvris", "MeWithoutYou", "Blindside"]
+  favorite_bands = ["wow", "Nirvana", "Pvris", "MeWithoutYou", "Blindside"]
   favorite_bands.shift
 end
 
