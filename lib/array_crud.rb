dc5c29eb826233fcd72851363cf8afa5a73547c8
Steph W <wilhelm.steph@gmail.com>
2020-03-24 22:55:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  favorite_bands = ["Nirvana", "Pvris", "MeWithoutYou", "Blindside"]
+  favorite_bands = ["Nirvana", "Pvris", "MeWithoutYou", "Blindside", "arrays!"]
   favorite_bands.last
 end
